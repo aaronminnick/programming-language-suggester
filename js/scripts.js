26b7875ec2ab016b9f99efc39f8ee17f59e5c28b
Aaron Minnick <abminnick@gmail.com>
2021-10-22 15:47:18 -0700

    add name entry and results styling, check for spelling/grammar

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
 // //Business Logic
-// function tallyUp(array) {
-//   let goPoints = 0;
-//   let pyPoints = 0;
-//   let swPoints = 0;
+function tallyUp(array) {
+  let goPoints = 0;
+  let pyPoints = 0;
+  let swPoints = 0;
 
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === "go") {
-//       goPoints += 1;
-//     } else if (array[i] === "python") {
-//       pyPoints += 1;
-//     } else if (array[i] === "swift") {
-//       swPoints +=1;
-//     }
-//   }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "go") {
+      goPoints += 1;
+    } else if (array[i] === "python") {
+      pyPoints += 1;
+    } else if (array[i] === "swift") {
+      swPoints +=1;
+    }
+  }
 
-//   if (goPoints > pyPoints && goPoints > swPoints) {
-//     return "Go";
-//   } else if (pyPoints > goPoints && pyPoints > swPoints) {
-//     return "Python";
-//   } else if (swPoints > goPoints && swPoints > pyPoints) {
-//     return "Swift";
-//   }
-// }
+  if (goPoints > pyPoints && goPoints > swPoints) {
+    return "Go";
+  } else if (pyPoints > goPoints && pyPoints > swPoints) {
+    return "Python";
+  } else if (swPoints > goPoints && swPoints > pyPoints) {
+    return "Swift";
+  }
+}
 
 
 //UI logic
 $(document).ready(function() {
+  $(".first-question").click(function() {
+    firstResponse = $("input:radio[name=question1]:checked").val();
+    if (firstResponse === "seat") {
+      $("#question2").prepend("<p>\"Thank you for agreeing to cooperate,\" the Inspector says. \"This shouldn\'t take much of your time.\"</p>")
+    } else if (firstResponse === "no-seat") {
+      $("#question2").prepend("<p>The Inspector stares at you, nonplussed. \"This will go much easier for both of us if you cooperate,\" they say flatly. Bland as the delivery is, you can hear the implied threat.</p>")
+    }
+  });
+
   $(".next-question").click(function() {
     $(this.parentElement).slideToggle();
     $(this.parentElement.nextElementSibling).slideToggle();
@@ -33,12 +42,14 @@ $(document).ready(function() {
   
   $("#quiz-form").submit(function(event) {
     event.preventDefault();
-  //   const answers[];
-  //   answers[0] = $("input:radio[name=question2]:checked").val();
-  //   answers[1] = $("input:radio[name=question3]:checked").val();
-  //   answers[2] = $("input:radio[name=question4]:checked").val();
-  //   answers[3] = $("input:radio[name=question5]:checked").val();
+    let answers = [];
+    let result;
 
-  //   result = tallyUp(answers);
+    answers[0] = $("input:radio[name=question2]:checked").val();
+    answers[1] = $("input:radio[name=question3]:checked").val();
+    answers[2] = $("input:radio[name=question4]:checked").val();
+    answers[3] = $("input:radio[name=question5]:checked").val();
+    result = tallyUp(answers);
+
   });
 });
