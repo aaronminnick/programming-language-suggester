0db3e8a90c6d2e596fcb7c3964b4632503ad1f61
Aaron Minnick <abminnick@gmail.com>
2021-10-22 12:23:50 -0700

    add attributes for all inputs, begin js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
 //Business Logic
+function tallyUp(array) {
+  let goPoints = 0;
+  let pyPoints = 0;
+  let swPoints = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "go") {
+      goPoints += 1;
+    } else if (array[i] === "python") {
+      pyPoints += 1;
+    } else if (array[i] === "swift") {
+      swPoints +=1;
+    }
+  }
+
+  if (goPoints > pyPoints && goPoints > swPoints) {
+    return "go";
+  } else if (pyPoints > goPoints && pyPoints > swPoints) {
+    return "python";
+  } else if (swPoints > goPoints && swPoints > pyPoints) {
+    return "swift";
+  }
+}
 
 
 //UI logic
@@ -10,5 +33,13 @@ $(document).ready(function() {
   
   $("#quiz-form").submit(function(event) {
     event.preventDefault();
+    const answers[];
+    answers[0] = $("input:radio[name=question2]:checked").val();
+    answers[1] = $("input:radio[name=question3]:checked").val();
+    answers[2] = $("input:radio[name=question4]:checked").val();
+    answers[3] = $("input:radio[name=question5]:checked").val();
+
+    tallyUp(answers);
+    }
   });
 });
